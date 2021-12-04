91eedfcb0e231dbadc9e8cfd94707c74941e2481
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2021-12-04 18:38:38 +0530

    fix: fix the issue damn it

--- index.js
+++ index.js
@@ -13,19 +13,29 @@ async function run () {
     
     const octokit = github.getOctokit(token)
 
-    console.log(octokit)
     
-    const { context = {}} = github;
+    const context = github.context;
 
-    const {pull_request} = context.payload
+    const pull_request_number = context.payload.pull_request.number;
 
-    await octokit.issues.createComment({
-        ...context.repo,
-        issue_number: pull_request.number,
-        body: 'Thank you for creating the PR 👍 😅'        
-    })
+    if (context.payload.pull_request == null) {
+        core.setFailed('Not linked to a pull request');
+        return;
+    }
+
+   
+
+    await octokit.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request_number, body: "Thank you for creating the pull request" }))
 
     console.log(context.payload)
 }
 
 run ();
+
+
+// const message = core.getInput('message');
+//             const github_token = core.getInput('GITHUB_TOKEN');
+//             const context = github.context;
+
+//             const octokit = new github.GitHub(github_token);
+//             const new_comment = octokit.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request_number, body: message }));
