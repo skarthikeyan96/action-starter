3d01efa2b8a3b8d1283e4977e7ebd3690c3fd182
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2021-12-04 18:41:40 +0530

    fix it

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ async function run () {
         return;
     }
 
+    console.log(octokit.issues)
    
 
     await octokit.issues.createComment(Object.assign(Object.assign({}, context.repo), { issue_number: pull_request_number, body: "Thank you for creating the pull request" }))
